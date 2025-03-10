55017d4023efb54a401aad7cfb9402e550942a94
Jun Park <90596660+CA-JunPark@users.noreply.github.com>
2025-03-10 11:25:37 -0700

    Set up for react-native-draggable-flatlist

--- TodoWidget/app/_layout.tsx
+++ TodoWidget/app/_layout.tsx
@@ -2,24 +2,27 @@ import { Stack } from "expo-router";
 import { MD3DarkTheme, PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";  
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 
 export default function RootLayout() {
   return (
-    
     <SafeAreaProvider>
-      <PaperProvider theme={MD3DarkTheme}>
-        <StatusBar
-          backgroundColor={MD3DarkTheme.colors.background}
-        />
-        <Stack> 
-          <Stack.Screen name="index" 
-            options={{ title: "Todo List", 
-            headerStyle: { backgroundColor: MD3DarkTheme.colors.primary }, 
-            headerTitleStyle: { color: MD3DarkTheme.colors.background },
-            headerTintColor: MD3DarkTheme.colors.background
-        }} /> 
-        </Stack>
-      </PaperProvider>
+      <GestureHandlerRootView>
+        <PaperProvider theme={MD3DarkTheme}>
+          <StatusBar
+            backgroundColor={MD3DarkTheme.colors.background}
+          />
+          <Stack> 
+            <Stack.Screen name="index" 
+              options={{ title: "Todo List", 
+              headerStyle: { backgroundColor: MD3DarkTheme.colors.primary }, 
+              headerTitleStyle: { color: MD3DarkTheme.colors.background },
+              headerTintColor: MD3DarkTheme.colors.background
+          }} /> 
+          </Stack>
+        </PaperProvider>
+      </GestureHandlerRootView>
     </SafeAreaProvider>
   );
 }
